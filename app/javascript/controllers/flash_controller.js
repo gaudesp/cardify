5fab990a77d99375e3abe1ff91287a1792da6dcd
gaudesp <gaudespro@gmail.com>
2025-08-03 16:22:20 +0200

    ⏱️ Increase flash delay

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    setTimeout(() => this.close(), 8000)
+    setTimeout(() => this.close(), 5000)
   }
 
   close() {
