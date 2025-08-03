eb3a6fa3fa9dbee4f1d440658ad1533768ef50b4
gaudesp <gaudespro@gmail.com>
2025-08-03 13:55:20 +0200

    🔒 Restrict admin access to site owner

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -4,5 +4,12 @@ module Admin
     before_action :authenticate_user!
 
     include LoadSiteConcern
+    before_action :autorize_user!
+
+    private
+
+    def autorize_user!
+      head :forbidden unless current_site.user_id == current_user.id
+    end
   end
 end
